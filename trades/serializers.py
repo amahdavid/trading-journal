6abb00570b8537ab56441ac25fe08041fccf3db6
David Chika Amah-Nnachi <78812828+amahdavid@users.noreply.github.com>
2026-09-06 09:35:03 -0700

    feat: add trade serializers

--- trades/serializers.py
+++ trades/serializers.py
@@ -1,1 +1,23 @@
-"""DRF serializers will be implemented here."""
+from rest_framework import serializers
+
+from .models import StrategyRule, Trade, TradeNote
+
+
+class TradeNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TradeNote
+        fields = "__all__"
+
+
+class TradeSerializer(serializers.ModelSerializer):
+    notes = TradeNoteSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Trade
+        fields = "__all__"
+
+
+class StrategyRuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StrategyRule
+        fields = "__all__"
