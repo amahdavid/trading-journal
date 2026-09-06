20f5aee53606e990150643ad00a25e5c87c4c1e7
David Chika Amah-Nnachi <78812828+amahdavid@users.noreply.github.com>
2026-09-06 09:35:07 -0700

    feat: add trade API viewsets

--- trades/views.py
+++ trades/views.py
@@ -1,1 +1,14 @@
-"""Trade API views will be implemented here."""
+from rest_framework import viewsets
+
+from .models import StrategyRule, Trade
+from .serializers import StrategyRuleSerializer, TradeSerializer
+
+
+class TradeViewSet(viewsets.ModelViewSet):
+    queryset = Trade.objects.all().order_by("-entry_date")
+    serializer_class = TradeSerializer
+
+
+class StrategyRuleViewSet(viewsets.ModelViewSet):
+    queryset = StrategyRule.objects.all()
+    serializer_class = StrategyRuleSerializer
